dfb20284621cd13db747a5cade41b43a0e6c14a3
Natha Biju <natha.biju@mail.waldenu.edu>
2023-02-21 14:15:56 +0530

    test2

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-    entry: "./admin/index.js",
-    output: {
-        path: `${__dirname}/admin`,
-        filename: "bundle.js"
-    },
     module: {
-        loaders: [
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  };
+    module: {
+        rules: [
             { test: /\.css$/, loader: "style-loader!css-loader" },
             {
                 test: /\.(eot|svg|ttf|woff|woff2)$/,
                 loader: 'file-loader?name=public/fonts/[name].[ext]'
             }
         ]
-    }
-};
+    };
